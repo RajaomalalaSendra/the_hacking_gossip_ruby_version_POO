78496c79687f74d4ff77c12d3edd0cd9fe162ee4
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-23 14:23:36 +0300

    modifying gossip

--- lib/controller.rb
+++ lib/controller.rb
@@ -3,12 +3,13 @@ require_relative "view"
 
 # The class of Controller
 class Controller
+  attr_accessor :author, :content
   def initialize
   	@view = View.new
   end
   def create_gossip
   	params = @view.create_gossip #pour le moment, le contenu du gossip est en dur dans le code. L'utilisateur ne peut pas le changer.
-    params.save
+    save_param = Gossip.new(params[:author],params[:content]).save
   end
 end
 ray  = Controller.new.create_gossip
--- lib/view.rb
+++ lib/view.rb
@@ -1,7 +1,11 @@
 require_relative "gossip"
 class View
-  attr_accessor
+  attr_accessor :author_view, :content_view
   def create_gossip
-	
+  	puts "Autheur: "
+  	@author_view = gets.chomp()
+  	puts "Content: "
+  	@content_view = gets.chomp()
+  	return params = { content: @content_view, author: @author_view }
   end
 end
